b1ef243e03c3475497bcb2edb16e55fa2450a367
UP2018781 <91746177+UP2018781@users.noreply.github.com>
2021-10-26 15:04:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,64 @@ function sn() {
 }
 
 // add your functions here
+
+function replaceText(elem,str){
+    elem.textContent = str;
+}
+
+function addTextTo(elem,str){
+    elem.append(str);
+}
+
+function moreBears(){
+    const bearElem = document.querySelector("#animals");
+    bearElem.src='http://placebear.com/400/200';
+    bearElem.alt='A bear.';
+    bearElem.title='A BEAR!';
+}
+
+function setId(elem,str){
+    elem.id=str;
+    return elem;
+}
+
+function setClass(elem,str){
+    elem.classList = str;
+    return elem;
+}
+
+function addAClass(elem,str){
+    elem.classList.add(str);
+    return elem;
+}
+
+function removeAClass(elem,str){
+    elem.classList.remove(str);
+    return elem;
+}
+
+function newElement(name){
+    const a = document.createElement(name);
+    return a;
+}
+
+function findElementById(id){
+    return  document.getElementById(id);
+}
+
+function findElementsByQuery(query){
+    return  document.querySelectorAll(query);
+}
+
+function reverseList(query){
+
+    const listhold = document.querySelectorAll(query)[0];
+    const L = listhold.children.length;
+    for (let i = L; i<L; i--){
+        console.log(listhold.append(listhold.children[i]));
+        listhold.append(listhold.children[i]);
+        
+    }
+
+
+}
